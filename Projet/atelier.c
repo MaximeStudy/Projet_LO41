7de5a227df79a989a72d30bac3fc50f677562693
study <maxime.study@gmail.com>
2016-12-03 09:40:17 +0100

    Ajout d'un makefile

---
+++ Projet/atelier.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Hello(void) {
+  printf("Hello, it's me lalalalalalalalalalalaaaa\n");
+}
---
+++ Projet/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "atelier.h"
+
+int main()
+{
+  Hello();
+  return EXIT_SUCCESS;
+}
